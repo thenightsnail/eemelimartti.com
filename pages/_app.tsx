6a8949c384a1dab07ec7ea91bad624bfe80d3d27
Eemeli Martti <felix.martti@gmail.com>
2022-01-04 21:29:28 +0200

    Remove pages

--- pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import React from 'react';
-import 'semantic-ui-css/semantic.min.css';
-
-const MyApp = ({ Component, pageProps }): JSX.Element => <Component {...pageProps} />;
-
-export default MyApp;
